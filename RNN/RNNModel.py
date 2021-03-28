9cf129b2b61c786ff300d8aec9ffed8ffa62c10f
ma2jemal <muna.aman66@gmail.com>
2021-03-28 06:45:41 -0400

    Tuned the model

--- RNN/RNNModel.py
+++ RNN/RNNModel.py
@@ -11,34 +11,21 @@ from keras.optimizers import Adam
 
 
 def RNN_model():
-    # SEG_LENTH = 180
-    # model = Sequential()
-    # model.add(Masking(mask_value= [0,0,0,0,0,0,0,0,0,0,0,0],\
-    #          input_shape=(SEG_LENTH, 12)))
-    # model.add(LSTM(128,dropout=0.2, recurrent_dropout=0.2,\
-    #               # kernel_regularizer = regularizers.l2(0.1),\
-    #                input_shape = (SEG_LENTH, 12),return_sequences = True))
-    # model.add(LSTM(64,dropout=0.2, recurrent_dropout=0.2,\
-    #               # kernel_regularizer = regularizers.l2(0.1),
-    #                return_sequences = True))
-    # model.add(LSTM(2))
-    # model.add(Activation('softmax'))
-    # adam = Adam()
-    # model.compile(loss = 'categorical_crossentropy',optimizer = adam,\
-    #               metrics = ['accuracy'])
+    SEG_LENTH = 180
     model = Sequential()
-    model.add(LSTM(32,input_shape=(180,12),recurrent_dropout=0.3,return_sequences=True ))
-    model.add(LSTM(64, return_sequences=True, recurrent_dropout=0.3, ))
-    model.add(LSTM(64))
-    model.add(BatchNormalization(momentum=0.9, epsilon=1e-5, axis=1))
-    model.add(Dropout(0.3))
-    model.add(Dense(64))
-    model.add(Activation('relu'))
-    model.add(BatchNormalization(momentum=0.9, epsilon=1e-5, axis=1))
-    model.add(Dropout(0.3))
-    model.add(Dense(2))
+    model.add(Masking(mask_value= [0,0,0,0,0,0,0,0,0,0,0,0],\
+             input_shape=(SEG_LENTH, 12)))
+    model.add(LSTM(128,dropout=0.2, recurrent_dropout=0.2,\
+                  # kernel_regularizer = regularizers.l2(0.1),\
+                   input_shape = (SEG_LENTH, 12),return_sequences = True))
+    model.add(LSTM(64,dropout=0.2, recurrent_dropout=0.2,\
+                  # kernel_regularizer = regularizers.l2(0.1),
+                   return_sequences = True))
+    model.add(LSTM(2))
     model.add(Activation('softmax'))
-    model.compile(loss='categorical_crossentropy', optimizer='adam')
+    adam = Adam()
+    model.compile(loss = 'categorical_crossentropy',optimizer = adam,\
+                  metrics = ['accuracy'])
 
     print(model.summary())
     return model 
