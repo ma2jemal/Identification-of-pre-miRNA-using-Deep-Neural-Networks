dc68b80edb1564d7e67f4fd861e00807a41d8399
ma2jemal <muna.aman66@gmail.com>
2021-03-28 06:35:26 -0400

    Tuned the model

--- RNN/RNNModel.py
+++ RNN/RNNModel.py
@@ -4,26 +4,42 @@
 import keras
 from keras import regularizers
 from keras.models import Sequential
-from keras.layers import Dense,Activation,Dropout
-from keras.layers import LSTM,Masking,Embedding
+from keras.layers import Activation, Dropout, Dense, BatchNormalization
+from keras.layers import LSTM,Masking
 from keras.optimizers import Adam
 
+
+
 def RNN_model():
-    SEG_LENTH = 180
+    # SEG_LENTH = 180
+    # model = Sequential()
+    # model.add(Masking(mask_value= [0,0,0,0,0,0,0,0,0,0,0,0],\
+    #          input_shape=(SEG_LENTH, 12)))
+    # model.add(LSTM(128,dropout=0.2, recurrent_dropout=0.2,\
+    #               # kernel_regularizer = regularizers.l2(0.1),\
+    #                input_shape = (SEG_LENTH, 12),return_sequences = True))
+    # model.add(LSTM(64,dropout=0.2, recurrent_dropout=0.2,\
+    #               # kernel_regularizer = regularizers.l2(0.1),
+    #                return_sequences = True))
+    # model.add(LSTM(2))
+    # model.add(Activation('softmax'))
+    # adam = Adam()
+    # model.compile(loss = 'categorical_crossentropy',optimizer = adam,\
+    #               metrics = ['accuracy'])
     model = Sequential()
-    model.add(Masking(mask_value= [0,0,0,0,0,0,0,0,0,0,0,0],\
-             input_shape=(SEG_LENTH, 12)))
-    model.add(LSTM(128,dropout=0.2, recurrent_dropout=0.2,\
-                  # kernel_regularizer = regularizers.l2(0.1),\
-                   input_shape = (SEG_LENTH, 12),return_sequences = True))
-    model.add(LSTM(64,dropout=0.2, recurrent_dropout=0.2,\
-                  # kernel_regularizer = regularizers.l2(0.1),
-                   return_sequences = True))
-    model.add(LSTM(2))
+    model.add(LSTM(32,input_shape=(180,12),recurrent_dropout=0.3,return_sequences=True ))
+    model.add(LSTM(64, return_sequences=True, recurrent_dropout=0.3, ))
+    model.add(LSTM(64))
+    model.add(BatchNormalization(momentum=0.9, epsilon=1e-5, axis=1))
+    model.add(Dropout(0.3))
+    model.add(Dense(64))
+    model.add(Activation('relu'))
+    model.add(BatchNormalization(momentum=0.9, epsilon=1e-5, axis=1))
+    model.add(Dropout(0.3))
+    model.add(Dense(2))
     model.add(Activation('softmax'))
-    adam = Adam()
-    model.compile(loss = 'categorical_crossentropy',optimizer = adam,\
-                  metrics = ['accuracy'])
+    model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
+
     print(model.summary())
     return model 
 
