d4b028a32cd9b9239e8b1402ca13fcf8fdbe6232
ma2jemal <muna.aman66@gmail.com>
2021-04-06 10:55:34 -0400

    load_model

--- CNN/CNNEvaluation.py
+++ CNN/CNNEvaluation.py
@@ -30,11 +30,11 @@ def predict_comparision(y_predict,y_test):
 # calculate SENS, SPEC, ACC and Matthews Correlation Coefficient (MCC)
 def test_evaluation(model_path,x_test_dataset, y_test_dataset):
     print("load the model")
-    # try:
-    model = load_model(model_path)
-    # except Exception:
-    #     print("The model file doesn't exist!")
-    #     exit(1)
+    try:
+         model = load_model(model_path)
+    except Exception:
+        print("The model file doesn't exist!")
+        exit(1)
     predict_result = model.predict(x_test_dataset)
 #     calculate tp,tn,fp,fn
     tp,tn,fp,fn = predict_comparision(predict_result,y_test_dataset)
